4f5c967744d6b7b91f62216ac856656af8fe0734
phil helm <phelm@metlife.com>
2016-07-26 21:08:48 -0400

    add eureka client

--- configuration_server/src/main/java/sample/configuration/ConfigurationServer.java
+++ configuration_server/src/main/java/sample/configuration/ConfigurationServer.java
@@ -4,12 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @SpringBootApplication //spring boot, auto config, and external variables (use @Value)
 @EnableConfigServer
 @EnableAutoConfiguration
+@EnableEurekaClient
+
 public class ConfigurationServer {
 	
 	public static void main(String[] args) throws Exception {
